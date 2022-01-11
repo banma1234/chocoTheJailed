63392cab537887a31ad1b5166611c282e4477d56
bumsu_notebook <banma1234@gmail.com>
2022-01-11 15:46:32 +0900

    save as insurance

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 
-const Section = styled.div`
+const MainSection = styled.div`
   position: absolute;
-  width: 100%; height: 100vh;    left: 250px;
-  font-size: 50px;
+  width: 85vw; height: 100vh;    left: 250px;  right: 250px;
+  text-align: center;
 `;
+
+const SlideBtn = styled.button`
+  width: 100px;  heigth: 100px;
+  border-radious: 100%;
+`
 
 function Main(){
     return(
-        <Section>Main</Section>
+        <MainSection>
+          <h1>Hello CtJ World!</h1>
+            <Swiper className = "banner"
+            spacebetween = { 50 }
+            slidesPerView = { 1 }
+            navigation
+            pagination = { {clickable : true} }></Swiper>
+        </MainSection>
     )
+}
+
+const nextBtn = () => {
+  
+}
+
+const prevBtn = () => {
+
 }
 
 export default Main;
