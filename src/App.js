a8fa01f5904eef1560bea425abb6cfc767c9eb92
banma1234 <banma1234@gmail.com>
2022-01-13 01:28:51 +0900

    피곤하네 증말~

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import SideBar from "./layOut/SideBar/SideBar";
+import SideBar from "./layOut/SideBar";
 import SnsBar from "./layOut/snsBar";
 import { Main, Profile, Like, Settings, NotFound } from "./pages";
 import './App.css';
@@ -23,23 +23,5 @@ function App() {
     </div>  // App
   );
 }
-
-// function Modal(){
-//   let [poly, polyChange] = useState();
-//   let [dollor, dollorChange] = useState(0);
-//   const onChange = (event)=>{
-//     dollorChange((event.target.value)*3);
-//   }
-//   return(
-//     <div className = "modal">
-//       <label htmlFor = "poly">polygon : </label>
-//       <input id = "poly" placeholder = "polygon"
-//       value = { poly } onChange={onChange}></input>
-//       <h4>?��(*°?��°*)?�� polygon convert to...</h4>
-//       <label htmlFor = "dollor">$ dollor : </label>
-//       <input id = "dollar" value = {dollor}></input>
-//     </div>
-//   )
-// }
 
 export default App;
