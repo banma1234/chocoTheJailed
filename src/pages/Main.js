acafa0760eb03346c4ce20f914bbab6f7b8221a9
banma1234 <banma1234@gmail.com>
2022-01-12 22:27:23 +0900

    커밋 해보라매 ㅋㅋ

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,36 +1,49 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from "styled-components";
 
+import "swiper/swiper.scss";
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+SwiperCore.use([Navigation, Pagination, Autoplay])
+
 const MainSection = styled.div`
-  position: absolute;
+  position: relative;
   width: 85vw; height: 100vh;    left: 250px;  right: 250px;
   text-align: center;
 `;
 
-const SlideBtn = styled.button`
-  width: 100px;  heigth: 100px;
-  border-radious: 100%;
-`
+const swiperStyle = {
+  position: "relative",
+  width: "1000px",  height: "1000px"
+};
+
+const SlideImg = [
+  { title: "slide_1", path: "img/slide_1.png" },
+  { title: "slide_2", path: "img/slide_2.png" },
+  { title: "slide_3", path: "img/slide_3.png" }
+];
+
+SwiperCore.use([Navigation, Pagination, Autoplay])
 
 function Main(){
     return(
         <MainSection>
           <h1>Hello CtJ World!</h1>
-            <Swiper className = "banner"
+          <Swiper style = {swiperStyle}
             spacebetween = { 50 }
             slidesPerView = { 1 }
             navigation
-            pagination = { {clickable : true} }></Swiper>
+            loop
+            autoplay = { {delay : 3000} }
+            pagination = { {clickable : true} }>
+            {SlideImg.map((index) => <SwiperSlide>
+              <img src = { index.path } alt = { index.title }></img></SwiperSlide>)}
+          </Swiper>
         </MainSection>
     )
-}
-
-const nextBtn = () => {
-  
-}
-
-const prevBtn = () => {
-
 }
 
 export default Main;
