3726c0a03e073c34072d225c233bb9da6056aaf2
Kanghaseong <hskang200@gmail.com>
2022-01-14 11:17:05 +0900

    fix Cards

--- src/pages/Cards.js
+++ src/pages/Cards.js
@@ -2,21 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 function Cards() {
-    const card = [
-        {
-            title: "Im title of img!!", 
-            img: "img/slide_1.png", 
-            expalin: "explain about this img", 
-            date: "2022/01/14"
-        }
-    ];
-
+    const cards = {
+        card: [
+            {
+                title: "Im title of img!!",
+                img: "img/slide_1.png",
+                expalin: "explain about this img",
+                date: "2022/01/14"
+            }
+        ]
+    }
     return (
         <div className="cards-container">
-            <img src={card.img}></img>
-            <strong>{card.title}</strong>
-            <div>{card.expalin}</div>
-            <div>{card.date}</div>
+            <img src={cards.card.img}></img>
+            <strong>{cards.card.title}</strong>
+            <div>{cards.card.expalin}</div>
+            <div>{cards.card.date}</div>
         </div>
     )
 }
