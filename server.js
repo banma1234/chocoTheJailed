71fea7cf61107558bf218150d90332b23ac7bffd
banma1234 <banma1234@gmail.com>
2022-01-16 13:50:05 +0900

    Choco the Jailed --version : onServer

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.listen(8080, () => {
+    console.log('server has been launched');
+})
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
+
+app.get('*', function(req, res){    // 리액트 Router 사용시 필요한 코드
+    res.sendFile(path.join(__dirname, 'build/index.html'));
+});
